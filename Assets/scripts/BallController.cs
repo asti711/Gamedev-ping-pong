3723da95dfda90e01872abac061d4cfc7e000db0
asti711 <aiganyshorozbakova@gmail.com>
2017-11-12 10:44:44 +0600

    Add ball random direction

--- Assets/scripts/BallController.cs
+++ Assets/scripts/BallController.cs
@@ -8,11 +8,12 @@ public class BallController : MonoBehaviour {
     public Rigidbody ballRigidBody;
     public Transform ballTransform; //determine position
     public bool isPlaying = false;
+    public Vector3 dirV;
 
 	// Use this for initialization
 	void Start () {
         ballTransform = this.transform;
-        BallMove();
+        BallDirection(0);
 	}
 
     // Update is called once per frame
@@ -24,9 +25,61 @@ public class BallController : MonoBehaviour {
         
 	}
 
+    public void BallDirection(int scoredPlayer)
+    {
+        if (scoredPlayer == 1)
+        {
+            ManageBallMove(1);
+        }
+        else if (scoredPlayer == 2)
+        {
+            ManageBallMove(-1);
+        }
+        else
+        {
+            ManageBallMove(0);
+        }
+
+    }
+
+    public void ManageBallMove(int dir)
+    {
+        float dirX = 0;
+        if (dir == 0)
+        {
+            dirX = GetRandomDirection();
+        }
+        else
+        {
+            dirX = dir;
+        }
+        float angleY = GetRandomAngle();
+        dirV = new Vector3(dirX, angleY);
+        BallMove();
+    }
+
+    public float GetRandomAngle()
+    {
+        return UnityEngine.Random.Range(0.25f, 0.75f);
+    }
+
+    public int GetRandomDirection()
+    {
+        int randomDir = 0;
+        if (UnityEngine.Random.Range(0, 2) == 0)
+        {
+            randomDir = -1;
+        }
+        else
+        {
+            randomDir = 1;
+        }
+        return randomDir;
+    }
+
     public void BallMove()
     {
-        ballRigidBody.AddForce(new Vector3(1, 0), ForceMode.Impulse);
+        ballRigidBody.AddForce(dirV, ForceMode.Impulse);
         isPlaying = true;
     }
 
@@ -37,7 +90,7 @@ public class BallController : MonoBehaviour {
             int nScoredPlayer = 0;
             isPlaying = false;
 
-            if(ballTransform.position.x < 0)
+            if (ballTransform.position.x < 0)
             {
                 nScoredPlayer = 2;
             }
@@ -49,19 +102,18 @@ public class BallController : MonoBehaviour {
             gameController.managePlayerScore(nScoredPlayer);
             //остановить и поставить снова на позицию
             ResetBall();
+
+            BallDirection(nScoredPlayer);
         }
     }
 
-    public void BallDirection()
-    {
-
-    }
+    
 
     public void ResetBall()
     {
         ballRigidBody.velocity = Vector3.zero;
         ballTransform.position = Vector3.zero;
 
-        BallMove();
+        
     }
 }
